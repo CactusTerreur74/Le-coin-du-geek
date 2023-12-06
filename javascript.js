df1576a2e23d0978ad077be6a9ddecec8938c29b
CactusTerreur74 <125867021+CactusTerreur74@users.noreply.github.com>
2023-12-06 18:11:39 +0100

    Index+css+js+theme modified

--- javascript.js
+++ javascript.js
@@ -1,25 +1,26 @@
-function Header() {
-    return (
-        <header>
-            <div className="title"><Link to={"/Astroid"}>Astroid</Link></div>
-            <button className="button" onClick={panel_click}><p>Click me</p></button>
-            <nav className="navbar"><Link to={"play"}>Play</Link><Link to={"credit"}>Credit</Link><ThemeChoice /></nav>
-        </header>
-    )
-}
-
-function panel_click () {
+function panel_click() {
     const button = document.getElementById("left-tab");
 
-    if (button.style.display == "none") {
-        button.style.display="flex";
-        document.getElementById("content").style.display="flex";
-        document.getElementById("content").style.paddingRight = document.getElementById("content")?.style.paddingTop
-    }
-    else {
+    if (button.style.display == "flex") {
         button.style.display="none";
         document.getElementById("content").style.display="block";
         document.getElementById("content").style.paddingRight=0;
     }
+    else {
+        button.style.display="flex";
+        document.getElementById("content").style.display="flex";
+        document.getElementById("content").style.paddingRight = document.getElementById("content")?.style.paddingTop
+    }
 
+}
+
+function theme_choice() {
+    const data_theme = document.documentElement.getAttribute("data-theme");
+    const button = document.getElementById("theme-img");
+
+    if (data_theme == "light") {
+        document.documentElement.setAttribute("data-theme", "dark");
+    } else {
+        document.documentElement.setAttribute("data-theme", "light");
+    }
 }
